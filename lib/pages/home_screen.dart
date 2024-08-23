70a16765837022f876056b608fb64d91b36ae337
egorterekhov <asya.wear@gmail.com>
2024-08-23 15:57:10 +0300

    pages layout

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../common/app_colors.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,13 +14,57 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(''),
+        backgroundColor: AppColors.backgroundPrimary,
+        centerTitle: false,
+        title: const Text(
+          'Home screen',
+          style: TextStyle(color: AppColors.white),
+        ),
       ),
-      body: const Center(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 30),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('Set valid API Base URL in order to continue'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.compare_arrows_sharp),
+                const SizedBox(
+                  width: 30,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextFormField(),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                    minWidth: MediaQuery.of(context).size.width * 0.9),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: AppColors.backgroundPrimary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    side: const BorderSide(
+                        width: 2, color: AppColors.cornerColor),
+                  ),
+                  child: const Text(
+                    'Start counting process',
+                    style: TextStyle(color: AppColors.black),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/process_screen.dart
+++ lib/pages/process_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../common/app_colors.dart';
+
 class ProcessScreen extends StatefulWidget {
   const ProcessScreen({super.key});
 
@@ -12,13 +14,49 @@ class _ProcessScreenState extends State<ProcessScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text(''),
+        backgroundColor: AppColors.backgroundPrimary,
+        centerTitle: false,
+        title: const Text(
+          'Process screen',
+          style: TextStyle(color: AppColors.white),
+        ),
       ),
-      body: const Center(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 30),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'All calculations has finished, you can send your results to server',
+            ),
+            const Divider(),
+            const Spacer(),
+            Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: MediaQuery.of(context).size.width * 0.9,
+                ),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: AppColors.backgroundPrimary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    side: const BorderSide(
+                      width: 2,
+                      color: AppColors.cornerColor,
+                    ),
+                  ),
+                  child: const Text(
+                    'Send results to server',
+                    style: TextStyle(color: AppColors.black),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
