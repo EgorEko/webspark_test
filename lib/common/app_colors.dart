2fb43559f9071728b3827e591f585ac1248d92ef
egorterekhov <asya.wear@gmail.com>
2024-08-23 15:55:53 +0300

    added common app colors

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundPrimary = Color.fromRGBO(0, 187, 255, 1.0);
+  static const cornerColor = Color(0xFF1976D2);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+}
