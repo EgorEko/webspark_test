3536acba9e9d27cc6b00bc010d226498df5a6c0e
egorterekhov <asya.wear@gmail.com>
2024-08-24 14:29:22 +0300

    changed app colors

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -5,4 +5,7 @@ class AppColors {
   static const cornerColor = Color(0xFF1976D2);
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF000000);
+  static const start = Color.fromRGBO(100, 255, 218, 1);
+  static const finish = Color.fromRGBO(0, 150, 136, 1);
+  static const shortcut = Color.fromRGBO(76, 175, 80, 1);
 }
